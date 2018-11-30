545bac168040ad61c4ef0fc23b989a0a8b0db5f0
Ken Roberts <kenmroberts@hotmail.com>
2018-11-30 15:24:45 -0500

    New splash screen, part III

--- VisionR4/My Project/AssemblyInfo.vb
+++ VisionR4/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.18.333.1")>
-<Assembly: AssemblyFileVersion("4.18.333.1")>
+<Assembly: AssemblyVersion("4.18.334.1")>
+<Assembly: AssemblyFileVersion("4.18.334.1")>
